4f2607bfae59c80386f5cc2e75271350866bc6e4
MakStef <stevaniwwasil@gmail.com>
2022-04-22 21:59:46 +0200

    Added templates

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def index(request):
+    data = {
+
+    }
+    return render(request, "main/index.html", data) 
