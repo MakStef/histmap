17db32d1a3e262c332b6c010b0d49112ea3a1f62
MakStef <stevaniwwasil@gmail.com>
2022-04-23 21:44:33 +0200

    Added model Level

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Level(models.Model):
+    land_name = models.CharField(max_length=150, verbose_name = "Назва історичного об'єкту")
+    since = models.IntegerField(verbose_name = "Рік створення")
+    source = models.FileField(verbose_name = "Файл рівня")
